5dbbea56b39da61d553b26103373d6fd3d025ca2
Sean Fu <sean14xx@gmail.com>
2020-08-05 17:01:48 +0200

    feat: Create User component
    - display a user information

--- src/App.js
+++ src/App.js
@@ -22,9 +22,18 @@ class UserList extends Component {
     return (
       <div>
         {users.map((user)=>(
-          <p>{user.username} played {user.game} {user.game>1 ? "games" : "game"}</p>
+          <User user={user}/>
         ))}
       </div>
+    )
+  }
+}
+
+class User extends Component {
+  render() {
+    const {user} = this.props;
+    return (
+      <p>{user.username} played {user.game} {user.game > 1 ? "games" : "game"}</p>
     )
   }
 }
