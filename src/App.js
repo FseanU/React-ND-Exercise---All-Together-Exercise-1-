c5e6ba348ee3443b4e505b097c3f8c5949095812
Sean Fu <sean14xx@gmail.com>
2020-08-05 16:56:01 +0200

    feat: Create UserList component
    - display a list of user information

--- src/App.js
+++ src/App.js
@@ -10,6 +10,25 @@ React and prepare you for your first project.
 The instructions for this project are located in the `instructions.md` file.
 */
 
+const users = [
+  {firstName: "Enrique", lastName: "Bruen", username: "Bart", game: 0},
+  {firstName: "Delilah", lastName: "Keebler", username: "Linnie", game: 3},
+  {firstName: "Chelsie", lastName: "Leannon", username: "Lexus", game: 14},
+  {firstName: "Charlie", lastName: "Langworth", username: "Miguel", game: 8},
+]
+
+class UserList extends Component {
+  render() {
+    return (
+      <div>
+        {users.map((user)=>(
+          <p>{user.username} played {user.game} {user.game>1 ? "games" : "game"}</p>
+        ))}
+      </div>
+    )
+  }
+}
+
 class App extends Component {
   render() {
     return (
@@ -18,6 +37,7 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">ReactND - Coding Practice</h1>
         </header>
+        <UserList />
       </div>
     );
   }
